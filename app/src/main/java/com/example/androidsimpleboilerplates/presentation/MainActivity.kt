c97cb280c84106b728eae86feca9aeef629390bc
shadikul.bari <shadikul.bari@bracits.com>
2022-10-04 10:50:05 +0600

    Initial commit

---
+++ app/src/main/java/com/example/androidsimpleboilerplates/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.androidsimpleboilerplates.presentation
+
+import android.os.Bundle
+import com.example.androidsimpleboilerplates.core.base.BaseActivity
+import com.example.androidsimpleboilerplates.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override fun getViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+    override fun getActivityName(): String = javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
